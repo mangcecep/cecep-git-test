88dc452ffee421f443d5658c7afae8a36ebbd952
mangcecep <cecepsolihinyusup@gmail.com>
2020-02-09 07:13:37 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
     <h1>Hello World</h1>
     <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nulla nemo ratione minus maxime ipsa, minima, a molestiae iure distinctio qui soluta corrupti sed quis? Labore ad fugit eius animi!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
